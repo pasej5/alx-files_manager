812c4d3d62db39e8ae98c88499ad78c02d5ce83f
Jealous Matsikachando <pasej5@gmail.com>
2024-03-09 02:23:26 +0200

    get value of a given key

--- utils/redis.js
+++ utils/redis.js
@@ -1,4 +1,5 @@
 import { createClient } from 'redis';
+import { get } from 'request';
 import { promisify } from 'utils';
 
 // class to define commands commonly used in redis
@@ -8,6 +9,21 @@ class RedisClient {
     this.client.on('error', (error) => {
       console.log(`Redis client not connected to server: ${error}`);
     });
+  }
+
+  // checking the redis connection and repoting the status
+  isAlive() {
+    if (this.client.connected) {
+      return true;
+    }
+    return false;
+  }
+
+  // get value for a given key
+  async get(key) {
+    const redisGet = promisify(this.client.get).bind(this.client);
+    const value = await redisGet(key);
+    return value;
   }
 }
 const redisClient = new RedisClient();
