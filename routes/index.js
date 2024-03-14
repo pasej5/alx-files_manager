17fc18ee416e031d9eb750418dc46414dae4b73c
bion-slmn <bionsol25@gmail.com>
2024-03-14 08:21:51 -0700

    eslint for route

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const AuthController = require('../controllers/AuthController');
 const FilesController = require('../controllers/FilesController');
 
 const app = Router();
-console.log(FilesController.putPublish, FilesController.putUnpublish)
+console.log(FilesController.putPublish, FilesController.putUnpublish);
 
 app.get('/status', AppController.getStatus);
 app.get('/stats', AppController.getStats);
@@ -20,7 +20,7 @@ app.post('/files', FilesController.postUpload);
 app.get('/files/:id', FilesController.getShow);
 app.get('/files', FilesController.getIndex);
 app.put('/files/:id/publish', FilesController.putPublish);
-app.put( '/files/:id/unpublish', FilesController.putUnpublish);
-app.get('/files/:id/data', FilesController.getFile)
+app.put('/files/:id/unpublish', FilesController.putUnpublish);
+app.get('/files/:id/data', FilesController.getFile);
 
 module.exports = app;
